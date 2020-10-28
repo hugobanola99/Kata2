65ed1eb4e798b8024f78831adf8f378f67337135
hugob <hugob@LAPTOP-SL46V4LI>
2020-10-28 14:12:39 +0000

    Kata2 version 4

--- src/kata2/Kata2.java
+++ src/kata2/Kata2.java
@@ -20,7 +20,7 @@ public class Kata2 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        int[] data = {1,3,4,6,7,3,4,5,1,3,7,6,3,1,4,7,6,8,3};
+        Integer[] data = {1,3,4,6,7,3,4,5,1,3,7,6,3,1,4,7,6,8,3};
         
         Histogram histo = new Histogram(data);
         Map<Integer,Integer> histogram = histo.getHistogram();
